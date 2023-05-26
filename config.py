84ff42036d5d091c0ec9d17ba17a5880a122967f
unknown <ricknogueira1231@gmail.com>
2023-05-26 17:49:14 -0300

    Tentativa de configuração do projeto

---
+++ config.py
@@ -0,0 +1,10 @@
+USERNAME='root'
+PASSWORD='123456'
+SERVER='localhost'
+DB='gerenciamento_contas'
+
+SQLALCHEMY_DATABASE_URI=f'mysql://{USERNAME}:{PASSWORD}@{SERVER}/{DB}'
+
+SQLALCHEMY_TRACK_MODIFICATIONS=True
+
+SECRET_KEY='chave_secreta1'
