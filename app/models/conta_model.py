01fffd1827a534cb868753ceb21732c57843a4ef
unknown <ricknogueira1231@gmail.com>
2023-05-30 15:15:57 -0300

    Executando Migrações

--- app/models/conta_model.py
+++ app/models/conta_model.py
@@ -1,7 +1,7 @@
 from app import db
 
 class Conta(db.Model):
-    __table__='conta'
+    __tablename__ ='conta'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     nome = db.Column(db.String(50), nullable=False)
