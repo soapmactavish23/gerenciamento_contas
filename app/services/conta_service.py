780fc8aec7dab101d3c961ffa688ba754c7f231f
unknown <ricknogueira1231@gmail.com>
2023-06-01 15:36:16 -0300

    Listando Contas

--- app/services/conta_service.py
+++ app/services/conta_service.py
@@ -1,6 +1,10 @@
 from app import db
 from ..models import conta_model
 
+def listar_contas():
+    contas = conta_model.Conta.query.all()
+    return contas
+
 def cadastrar_conta(conta):
     conta_bd = conta_model.Conta(nome=conta.nome, resumo=conta.resumo, valor=conta.valor)
     db.session.add(conta_bd)
