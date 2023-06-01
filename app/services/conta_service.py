e45cac13ea38cbb98f91d38fdcfb5937921079db
unknown <ricknogueira1231@gmail.com>
2023-06-01 15:42:52 -0300

    Listar Conta por Id

--- app/services/conta_service.py
+++ app/services/conta_service.py
@@ -5,6 +5,10 @@ def listar_contas():
     contas = conta_model.Conta.query.all()
     return contas
 
+def listar_conta_id(id):
+    conta = conta_model.Conta.query.filter_by(id=id).first()
+    return conta
+
 def cadastrar_conta(conta):
     conta_bd = conta_model.Conta(nome=conta.nome, resumo=conta.resumo, valor=conta.valor)
     db.session.add(conta_bd)
--- app/views/conta_view.py
+++ app/views/conta_view.py
@@ -23,5 +23,13 @@ class ContaList(Resource):
             resultado = conta_service.cadastrar_conta(conta_nova)
             return make_response(cs.jsonify(resultado), 201)
 
+class ContaDetail(Resource):
+    def get(self, id):
+        conta = conta_service.listar_conta_id(id)
+        if conta is None:
+            return make_response(jsonify("Conta não Encontrada"), 404)
+        cs = conta_schema.ContaSchema()
+        return make_response(cs.jsonify(conta), 200)
 
 api.add_resource(ContaList, '/contas')
+api.add_resource(ContaDetail, '/contas/<int:id>')
