e0c468945c2b2489043e9b2eee9f4659c2a10a0d
unknown <ricknogueira1231@gmail.com>
2023-06-06 15:49:21 -0300

    Criando a View

---
+++ app/views/operacao_view.py
@@ -0,0 +1,27 @@
+from flask_restful import Resource
+from ..schemas import operacao_schema
+from flask import request, make_response, jsonify
+from ..entidades import operacao
+from ..services import operacao_service
+from app import api
+
+class OperacaoList(Resource):
+    def post(self):
+        os = operacao_schema.OperacaoSchema()
+        validate = os.validate(request.json)
+        if validate:
+            return make_response(jsonify(validate), 400)
+        else:
+            nome = request.json["nome"]
+            resumo = request.json["resumo"]
+            custo = request.json["custo"]
+            tipo = request.json["tipo"]
+
+            operacao_nova = operacao.Operacao(
+                nome=nome,
+                resumo=resumo,
+                custo=custo,
+                tipo=tipo
+            )
+            resultado = operacao_service.cadastrar_operacao(operacao_nova)
+            return make_response(os.jsonify(resultado), 201)
