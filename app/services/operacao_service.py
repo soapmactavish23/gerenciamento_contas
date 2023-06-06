ab39561382609828099b7b093215f1f1969cfe58
unknown <ricknogueira1231@gmail.com>
2023-06-06 15:44:09 -0300

    Criando o Service

---
+++ app/services/operacao_service.py
@@ -0,0 +1,13 @@
+from app import db
+from ..models import operacao_model
+
+def cadastrar_operacao(operacao):
+    operacao_bd = operacao_model.Operacao(
+        nome=operacao.nome,
+        resumo=operacao.resumo,
+        custo=operacao.custo,
+        tipo=operacao.tipo
+    )
+    db.session.add(operacao_bd)
+    db.session.commit()
+    return operacao_bd
